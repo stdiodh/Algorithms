66a66090aff1dae3a15cd5f3347e2e7dc4bcf04e
stdiodh <playlistdh@gmail.com>
2025-03-19 09:42:30 +0900

    2week

---
+++ src/2week/GCDinterative.kt
@@ -0,0 +1,15 @@
+package `2week`
+
+fun main(){
+    fun gcdRecursive(a : Int, b : Int) : Int {
+        if(b == 0){
+            return a
+        }
+        return gcdRecursive(b, a % b)
+    }
+
+    val num1 = 24
+    val num2 = 14
+
+    print("최대공약수 : ${gcdRecursive(num1, num2)}")
+}
---
+++ src/2week/findMaximum.kt
@@ -0,0 +1,25 @@
+package `2week`
+
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int {
+        nextToken()
+        return nval.toInt()
+    }
+    fun findByMaximum(intArray: IntArray) : Int{
+        var max = intArray[0]
+        repeat(intArray.size){
+            if(intArray[it] > max){
+                max = intArray[it]
+            }
+        }
+        return max
+    }
+
+    val n = nextInt()
+    val intArray = IntArray(n){ nextInt() }
+    val max = findByMaximum(intArray)
+
+    print("최다값 : $max")
+}
