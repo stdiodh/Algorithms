f1560d4e46d5f0533f273f76c669710f57bdd1fe
stdiodh <playlistdh@gmail.com>
2025-04-09 12:05:42 +0900

    수박

---
+++ src/Programmers/Lv1/수박.kt
@@ -0,0 +1,15 @@
+package Programmers.Lv1
+
+class 수박 {
+    fun solution(n: Int): String {
+        var answer = ""
+        repeat(n){
+            if(it % 2 == 0){
+                answer += "수"
+            } else {
+                answer += "박"
+            }
+        }
+        return answer
+    }
+}
