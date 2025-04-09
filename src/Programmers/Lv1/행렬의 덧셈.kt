ce2b4d4dd5a1bcb73def3255b22de10c8370e700
stdiodh <playlistdh@gmail.com>
2025-04-09 16:53:54 +0900

    행렬의 덧셈

---
+++ src/Programmers/Lv1/행렬의 덧셈.kt
@@ -0,0 +1,13 @@
+package Programmers.Lv1
+
+class `행렬의 덧셈` {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> {
+        var answer = Array(arr1.size) { IntArray(arr1[0].size) { 0 } }
+        for(i in arr1.indices){
+            for(j in arr1[0].indices){
+                answer[i][j] = arr1[i][j] + arr2[i][j]
+            }
+        }
+        return answer
+    }
+}
