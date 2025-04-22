a8574f1ac3ae759328278d231f3e8055cc59120c
stdiodh <playlistdh@gmail.com>
2025-04-22 21:13:00 +0900

    1927

---
+++ src/BOJ/Structure/1927.kt
@@ -0,0 +1,24 @@
+import java.io.StreamTokenizer
+import java.util.*
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int { nextToken(); return nval.toInt() }
+    val n = nextInt()
+
+    val pq = PriorityQueue<Int>()
+    val sb = StringBuilder()
+
+    repeat(n) {
+        val num = nextInt()
+        if (num == 0) {
+            if (pq.isEmpty()) {
+                sb.appendLine(0)
+            } else {
+                sb.appendLine(pq.poll())
+            }
+        } else {
+            pq.offer(num)
+        }
+    }
+    print(sb)
+}
