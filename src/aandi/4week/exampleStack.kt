4473c65864beade88ead8bbf8787d1cacc18bc28
stdiodh <playlistdh@gmail.com>
2025-05-02 11:42:17 +0900

    exampleStack

---
+++ src/aandi/4week/exampleStack.kt
@@ -0,0 +1,19 @@
+package aandi.`4week`
+
+fun main(){
+    val a = 10
+    val b = 20
+    val sum = add(a, b)
+    print(sum)
+}
+
+fun add(a:Int, b:Int) : Int {
+    return a + b
+}
+//
+//data class User(val name: String)
+//
+//fun main() {
+//    val u1 = User("DH")
+//    val u2 = User("HD")
+//}
