ae00bec562d4c2ce4c2c0232802b6bf4dcc73153
stdiodh <playlistdh@gmail.com>
2025-06-05 16:05:06 +0900

    2485

---
+++ src/BOJ/DivMul/2485.kt
@@ -0,0 +1,29 @@
+package BOJ.DivMul
+
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int { nextToken(); return nval.toInt() }
+    fun gcd(a : Int, b : Int) : Int {
+        return if (b == 0){
+            a
+        } else {
+            gcd (b, a % b)
+        }
+    }
+
+    val n = nextInt()
+    val tree = IntArray(n) { nextInt() }
+    val diff = IntArray(n-1) { i -> tree[i+1] - tree[i] }
+
+    var currGcd = diff[0]
+    for(i in 1 until n-1){
+        currGcd = gcd(currGcd, diff[i])
+    }
+
+    var cnt = 0
+    for(j in diff){
+        cnt += j / currGcd - 1
+    }
+    print(cnt)
+}
