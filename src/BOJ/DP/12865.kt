99b76bd82270aa9fdb1052684757af2ee2acfbaa
stdiodh <playlistdh@gmail.com>
2025-06-11 15:01:43 +0900

    12865

--- src/BOJ/DP/12865.kt
+++ src/BOJ/DP/12865.kt
@@ -1,28 +1,43 @@
 package BOJ.DP
 
 import java.io.StreamTokenizer
-import kotlin.math.max
+//import kotlin.math.max
+
+//fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+//    fun nextInt() : Int { nextToken(); return nval.toInt() }
+//
+//    //n = 물품의 수 k = 버틸 수 있는 무게
+//    val n = nextInt(); val k = nextInt()
+//    val knap = Array(n+1) { IntArray(k+1) { 0 } }
+//    //first = 무게 second = 가치
+//    val thing = Array(n) { Pair(nextInt(), nextInt()) }
+//
+//    for (i in 1..n) {
+//        for (j in 0 until k) {
+//            val weight = thing[i - 1].first
+//            val value = thing[i - 1].second
+//            if (j + 1 >= weight) {
+//                knap[i][j + 1] = max(knap[i - 1][j + 1], knap[i - 1][j + 1 - weight] + value)
+//            } else {
+//                knap[i][j + 1] = knap[i - 1][j + 1]
+//            }
+//        }
+//    }
+//
+//    println(knap[n][k])
+//}
+
+//1차원 배열 개선 + 모법 답안
 
 fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
     fun nextInt() : Int { nextToken(); return nval.toInt() }
-
-    //n = 물품의 수 k = 버틸 수 있는 무게
     val n = nextInt(); val k = nextInt()
-    val knap = Array(n+1) { IntArray(k+1) { 0 } }
-    //first = 무게 second = 가치
-    val thing = Array(n) { Pair(nextInt(), nextInt()) }
-
-    for (i in 1..n) {
-        for (j in 0 until k) {
-            val weight = thing[i - 1].first
-            val value = thing[i - 1].second
-            if (j + 1 >= weight) {
-                knap[i][j + 1] = max(knap[i - 1][j + 1], knap[i - 1][j + 1 - weight] + value
-                )
-            } else {
-                knap[i][j + 1] = knap[i - 1][j + 1]
-            }
+    val dp = IntArray(k + 1)
+    for (i in 0 until n) {
+        val w = nextInt(); val v = nextInt()
+        for (j in k downTo w) {
+            dp[j] = maxOf(dp[j], dp[j - w] + v)
         }
     }
-    println(knap[n][k])
+    println(dp[k])
 }
