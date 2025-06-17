06a5ab73a914d698eaa3e9a3c5568947aba2bd01
stdiodh <playlistdh@gmail.com>
2025-06-17 14:21:58 +0900

    1037

---
+++ src/BOJ/sort/1037.kt
@@ -0,0 +1,19 @@
+package BOJ.DP
+
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun int() : Int {nextToken(); return nval.toInt()}
+
+    val n = int()
+    val list = mutableListOf<Int>()
+    repeat(n){
+        val num = int()
+        list.add(num)
+    }
+
+    val min = list.min()
+    val max = list.max()
+
+    println(min * max)
+}
