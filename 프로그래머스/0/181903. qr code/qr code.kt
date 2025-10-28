9760bf9f58f3a4569f08c05e720aee194e9d2ff1
Hood <120657616+stdiodh@users.noreply.github.com>
2025-10-29 08:55:11 +0900

    [level 0] Title: qr code, Time: 15.09 ms, Memory: 63.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(q: Int, r: Int, code: String): String {
+        var answer = ""
+        for(i in code.indices) {
+            if(i % q == r){
+                answer += code[i]
+            }
+        }
+        return answer
+    }
+}
