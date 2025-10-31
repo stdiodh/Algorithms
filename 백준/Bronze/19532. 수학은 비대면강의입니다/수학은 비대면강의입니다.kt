2364e514705306decd9090442dabc3c538f234e7
Hood <120657616+stdiodh@users.noreply.github.com>
2025-10-31 14:51:53 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 76 ms, Memory: 12284 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.kt
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.kt
@@ -5,12 +5,8 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
 
     val a1 = i(); val b1 = i(); val c1 = i(); val a2 = i(); val b2 = i(); val c2 = i()
 
-    for(x in -999 .. 999) {
-        for(y in -999 .. 999) {
-            if((a1 * x + b1 * y) == c1 && (a2 * x + b2 * y) == c2) {
-                print("$x $y")
-                break
-            }
-        }
-    }
+    val x = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+    val y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1)
+
+    println("$x $y")
 }
