bd34cd4f48792443afc20a5378694264329b744a
Hood <120657616+stdiodh@users.noreply.github.com>
2025-11-06 10:42:20 +0900

    [Silver V] Title: 배열 합치기, Time: 1368 ms, Memory: 297684 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.kt
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.kt
@@ -4,18 +4,8 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
     fun i() : Int { nextToken(); return nval.toInt() }
 
     val a = i(); val b = i()
-    val arr = mutableListOf<Int>()
+    val aArr = IntArray(a) { i() }
+    val bArr = IntArray(b) { i() }
 
-    repeat(a) {
-        val num = i()
-        arr.add(num)
-    }
-
-    repeat(b) {
-        val num = i()
-        arr.add(num)
-    }
-
-    arr.sort()
-    print(arr.joinToString(" "))
+    print((aArr + bArr).sortedArray().joinToString(" "))
 }
