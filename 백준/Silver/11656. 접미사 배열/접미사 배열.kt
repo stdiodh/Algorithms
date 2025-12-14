4fb52dd95df690ac6dccf928e8d0726310e00af2
Hood <120657616+stdiodh@users.noreply.github.com>
2025-12-14 23:53:15 +0900

    [Silver IV] Title: 접미사 배열, Time: 128 ms, Memory: 19816 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.kt
@@ -0,0 +1,16 @@
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextString() : String { nextToken(); return sval }
+
+    val s = nextString()
+    val sArr = mutableListOf<String>()
+    val sb = StringBuilder()
+
+    for(x in s.indices) {
+        sArr.add(s.substring(x, s.length))
+    }
+    sArr.sort()
+    sArr.forEach { sb.appendLine(it)}
+    print(sb)
+}
