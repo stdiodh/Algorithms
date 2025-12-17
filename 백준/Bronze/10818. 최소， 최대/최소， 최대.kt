18dac53a1eac2e80e4d5acbcddb8710ca2b11d51
Hood <120657616+stdiodh@users.noreply.github.com>
2025-12-17 14:39:00 +0900

    [Bronze III] Title: 최소, 최대, Time: 396 ms, Memory: 24744 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.kt
@@ -0,0 +1,12 @@
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int { nextToken(); return nval.toInt() }
+
+    val n = nextInt()
+    val arr = IntArray(n) { nextInt() }
+
+    val min = arr.minOf { it }
+    val max = arr.maxOf { it }
+    print("$min $max")
+}
