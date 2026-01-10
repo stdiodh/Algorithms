57c233df57f7266d7d4b81e4b7efe88e22bc73eb
Hood <120657616+stdiodh@users.noreply.github.com>
2026-01-10 13:02:38 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 88 ms, Memory: 12948 KB -BaekjoonHub

---
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.kt
@@ -0,0 +1,41 @@
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int { nextToken(); return nval.toInt() }
+
+    val n = nextInt()
+    val sb = StringBuilder()
+
+    sb.appendLine("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
+
+    fun printText(i : Int) {
+        if(i == n){
+            repeat(i * 4){ sb.append("_")}
+            sb.appendLine("\"재귀함수가 뭔가요?\"")
+            repeat(i * 4){ sb.append("_")}
+            sb.appendLine("\"재귀함수는 자기 자신을 호출하는 함수라네\"")
+            return
+        }
+        repeat(i * 4){ sb.append("_") }
+        sb.appendLine("\"재귀함수가 뭔가요?\"")
+
+        repeat(i * 4){ sb.append("_") }
+        sb.appendLine("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.")
+
+        repeat(i * 4){ sb.append("_") }
+        sb.appendLine("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
+
+        repeat(i * 4){ sb.append("_") }
+        sb.appendLine("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
+
+        return printText(i+1)
+    }
+    printText(0)
+
+    for (j in n downTo 0) {
+        repeat(j * 4) { sb.append("_") }
+        sb.appendLine("라고 답변하였지.")
+    }
+
+    println(sb)
+}
