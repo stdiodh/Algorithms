9a53bea73bcf059aa8f39c8b63b638d19c8c3368
Hood <120657616+stdiodh@users.noreply.github.com>
2026-01-14 16:54:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1172 ms, Memory: 96772 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
@@ -0,0 +1,19 @@
+import java.io.StreamTokenizer
+import java.util.ArrayList
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun nextInt() : Int { nextToken(); return nval.toInt() }
+    val bw = System.out.bufferedWriter()
+    val n = nextInt()
+    val arr = ArrayList<Int>(n)
+    val sb = StringBuilder()
+
+    repeat(n){
+        arr.add(nextInt())
+    }
+    arr.sort()
+
+    arr.forEach { sb.appendLine(it) }
+    bw.write(sb.toString())
+    bw.close()
+}
