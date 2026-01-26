d8ae1e70c48e9ba8943939e8244b35ae1bfac9b8
Hood <120657616+stdiodh@users.noreply.github.com>
2026-01-26 12:30:37 +0900

    [Silver II] Title: 스택 수열, Time: 240 ms, Memory: 23056 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.kt
@@ -0,0 +1,40 @@
+import java.io.StreamTokenizer
+import java.util.*
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
+    fun i() : Int { nextToken(); return nval.toInt() }
+
+    val n = i()
+    val arr = IntArray(n) { i() }
+    val stack = Stack<Int>()
+    val sb = StringBuilder()
+
+    var num = 1
+    var flag = true
+    for(i in 0 until n){
+        val su = arr[i]
+        if(su >= num){
+            while(su >= num){
+                stack.push(num++)
+                sb.appendLine("+")
+            }
+            stack.pop()
+            sb.appendLine("-")
+        } else {
+            val j = stack.pop()
+            if(j > su){
+                flag = false
+                break
+            } else {
+                sb.appendLine("-")
+            }
+        }
+
+    }
+    if(!flag){
+        print("NO")
+    } else {
+        print(sb)
+    }
+
+}
