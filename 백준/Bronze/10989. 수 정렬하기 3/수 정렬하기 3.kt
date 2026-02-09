e1d9d03181c394b8cf8228d592b6030111b11abd
Hood <120657616+stdiodh@users.noreply.github.com>
2026-02-09 14:09:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1536 ms, Memory: 300100 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.kt
@@ -0,0 +1,25 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = System.out.bufferedWriter()
+    val n = br.readLine().toInt()
+
+    // 배열 초기화
+    val list = IntArray(10001)
+
+    // 나타난 숫자 표시
+    for (i in 0 until n) {
+        list[br.readLine().toInt()]++
+    }
+
+    for (i in 1..10000) {
+        bw.write("$i\n".repeat(list[i]))
+    }
+
+    bw.flush()
+    bw.close()
+    br.close()
+}
