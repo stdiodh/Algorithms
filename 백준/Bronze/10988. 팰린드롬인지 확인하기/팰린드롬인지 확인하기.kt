5559283a280f799adc1f07e1b67a661c3d9d0bcc
Hood <120657616+stdiodh@users.noreply.github.com>
2026-03-19 17:38:15 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 80 ms, Memory: 12292 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.kt
@@ -1,14 +1,27 @@
 import java.io.StreamTokenizer
 
-fun main() = with(StreamTokenizer(System.`in`.bufferedReader())){
-    fun readInt() : Int { nextToken(); return nval.toInt() }
-    fun readString() : String { nextToken(); return sval }
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    fun readInt(): Int {
+        nextToken(); return nval.toInt()
+    }
+
+    fun readString(): String {
+        nextToken(); return sval
+    }
 
     val str = readString()
-    
-    if(str == str.reversed()) {
-        print("1")
-    } else {
-        print("0")
+    var start = 0
+    var end = str.length - 1
+    var flag = true
+    while (start < end) {
+        if (str[start] != str[end]) {
+            flag = false
+            break
+        }
+
+        start += 1
+        end -= 1
     }
+    
+    print(if(flag) 1 else 0)
 }
