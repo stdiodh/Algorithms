9b4464263593cb4723b08a463a7c9b46e8b26b9a
Hood <120657616+stdiodh@users.noreply.github.com>
2026-03-20 15:09:19 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 18440 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.kt
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.kt
@@ -1,5 +1,27 @@
-fun main() {
-    val br = System.`in`.bufferedReader()
-    val n = br.readLine().toCharArray().sortedDescending().joinToString("")
-    println(n)
+import java.io.StreamTokenizer
+
+fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
+    fun readInt(): Int {
+        nextToken(); return nval.toInt()
+    }
+
+    fun readString(): String {
+        nextToken(); return sval
+    }
+
+    val num = readInt().toString().toCharArray()
+
+    for(i in 0 .. num.size - 1) {
+        var minIdx = i
+        for(j in i until num.size) {
+            if(num[minIdx] < num[j]) {
+                minIdx = j
+            }
+        }
+        val temp = num[minIdx]
+        num[minIdx] = num[i]
+        num[i] = temp
+    }
+
+    print(num.joinToString(""))
 }
