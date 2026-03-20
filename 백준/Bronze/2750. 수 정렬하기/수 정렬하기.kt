35e178509425c920bc0eeaa3bb278367cbb610a6
Hood <120657616+stdiodh@users.noreply.github.com>
2026-03-20 14:41:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 132 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.kt
@@ -10,7 +10,19 @@ fun main() = with(StreamTokenizer(System.`in`.bufferedReader())) {
     }
 
     val n = readInt()
-    val intArr = IntArray(n) { readInt() }.sortedArray()
+    val intArr = IntArray(n) { readInt() }
+
+    for(i in 0 .. intArr.size - 1) {
+        var minIndex = i
+        for(j in i + 1 until intArr.size) {
+            if(intArr[j] < intArr[minIndex]) {
+                minIndex = j
+            }
+        }
+        val temp = intArr[minIndex]
+        intArr[minIndex] = intArr[i]
+        intArr[i] = temp
+    }
 
     intArr.forEach { println(it) }
 }
