0c70fce5c7fbac56dabc200d3630fd9e47439195
Hood <120657616+stdiodh@users.noreply.github.com>
2026-04-04 17:01:20 +0900

    [Silver IV] Title: 30, Time: 200 ms, Memory: 21120 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val n = br.readLine().toCharArray()
+    n.sortDescending()
+
+    // 0이 없거나, 3의 배수가 아닌 경우
+    if ('0' !in n || n.sumOf { it.digitToInt() } % 3 != 0) {
+        print(-1)
+    } else {
+        print(n.joinToString(""))
+    }
+}
