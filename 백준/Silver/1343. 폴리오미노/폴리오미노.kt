73df1e0c7eca41ec15a6a424de92c54b39b8d5ac
Hood <120657616+stdiodh@users.noreply.github.com>
2026-04-12 13:25:42 +0900

    [Silver V] Title: 폴리오미노, Time: 76 ms, Memory: 12328 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.kt
@@ -0,0 +1,36 @@
+fun main() {
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+
+    val line = br.readLine()
+    var str = StringBuilder()
+    var count = 0
+    for (i in 0 .. line.count()) {
+        if (i == line.count() || line[i] == '.') {
+            if (count % 2 == 1) {
+                str = StringBuilder()
+                str.append(-1)
+                break
+            }
+
+            while (count - 4 >= 0) {
+                str.append("AAAA")
+                count -= 4
+            }
+            while (count - 2 >= 0) {
+                str.append("BB")
+                count -= 2
+            }
+            if (i < line.count()) {
+                str.append(".")
+            }
+            count = 0
+        } else {
+            count += 1
+        }
+    }
+    bw.write(str.toString())
+    br.close()
+    bw.flush()
+    bw.close()
+}
